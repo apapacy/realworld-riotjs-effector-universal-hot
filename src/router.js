1b93900576de1f942e84891c434548f6c7c3c475
Андрей Овчаренко <ovcharenkoav@meta.ua>
2019-06-03 16:00:21 +0300

    client router

--- src/router.js
+++ src/router.js
@@ -1,9 +1,6 @@
 import UniversalRouter from 'universal-router';
 const riot = require('riot');
-const router = new UniversalRouter([
-  { path: '/1', action: () => riot.mount('#app', {}, 'page1') },
-  { path: '/2', action: () => riot.mount('page2') },
+export default new UniversalRouter([
+  { path: '/1', action: () => {'page1', {}} },
+  { path: '/2', action: () => {'page1', {}} },
 ])
-
-router.resolve({ pathname: '/1', user: 'admin' })
-  .then(result => alert(result))
