10a4446fbc6fcffa90b76a7635fdc9772ee62449
Андрей Овчаренко <ovcharenkoav@meta.ua>
2019-06-04 12:25:45 +0300

    add page routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import Layout from './riot/my-layout.riot'
 import hydrate from '@riotjs/hydrate'
 import './register';
-import {getInitialComponentName} from './router.client';
+import {getInitialComponentName, setRootComponent} from './router.client';
 
 const hydrateWithMyComponent = hydrate(Layout)
-alert(1)
-getInitialComponentName().then(component => {
-   alert(component)
-   hydrateWithMyComponent(
+getInitialComponentName().then(component => 
+    setRootComponent(hydrateWithMyComponent(
      document.getElementById('app'),
      {page: component[0]}
      //window.__INITIAL_APPLICATION_PROPS__
-   )
- });
+   ))
+ );
--- src/router.client.js
+++ src/router.client.js
@@ -5,18 +5,23 @@ import router from './router';
 const riot = require('riot')
 
 
+let root;
 let currentPage
+
 const render = async (location) => {
    alert('rebder')
     const route = await router.resolve(location);
-    currentPage = riot.identifyTagFromDom(document.getElementById("page"))
-    currentPage.unmount(true)
-    riot.mount(route[0])
+    root.update({page: route[0]})
 };
 
 history.listen(render);
 
 export function getInitialComponentName() {
   return router.resolve(history.location)
+}
+
+export function setRootComponent(component) {
+  root = component;
+  console.log(component)
 }
 // render(history.location);
