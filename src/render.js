d7e7ce167b32f32bc261cbb61a6e34bcc3554f39
Андрей Овчаренко <ovcharenkoav@meta.ua>
2019-06-07 14:18:42 +0300

    remove wrapper tag

--- src/render.js
+++ src/render.js
@@ -30,8 +30,8 @@ export const render = async function(req, res, next) {
           <!-- Import the custom Bootstrap 4 theme from our hosted CDN -->
           <link rel="stylesheet" href="//demo.productionready.io/main.css">
         </head>
-        <body id='app'>
-          ${html}
+        <body>
+          <section id='app'>${html.replace(/^<section[^>]*>|<\/section>$/ , '')}</section>
           <script src='/static/common.bundle.js' type='text/javascript'></script>
           <script src='/static/main.bundle.js' type='text/javascript'></script>
         </body>
