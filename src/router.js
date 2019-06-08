26bc389e08fe6be53d1f523f505ad4c973f6e9ab
apaapcy <apapacy@gmail.com>
2019-06-08 10:27:22 +0300

    route as object

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import UniversalRouter from 'universal-router';
 export default new UniversalRouter([
-  { path: '/sign-in', action: () => ({ page: 'login', action: 'sign-in' }) },
-  { path: '/sign-up', action: () => ({ page: 'login', action: 'sign-up' }) },
+  { path: '/sign-in', action: () => ({ page: 'login', data: { action: 'sign-in' } }) },
+  { path: '/sign-up', action: () => ({ page: 'login', data: { action: 'sign-up' } }) },
   { path: '(.*)', action: () => ({ page: 'page3' }) },
 ])
