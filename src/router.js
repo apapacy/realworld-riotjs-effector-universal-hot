e2e9e12f600cff79817b15eabbf02d56d3f91f71
Андрей Овчаренко <ovcharenkoav@meta.ua>
2019-06-10 18:21:24 +0300

    add tags route

--- src/router.js
+++ src/router.js
@@ -3,7 +3,8 @@ export default new UniversalRouter([
   { path: '/sign-in', action: () => ({ page: 'login', data: { action: 'sign-in' } }) },
   { path: '/sign-up', action: () => ({ page: 'login', data: { action: 'sign-up' } }) },
   { path: '/settings', action: () => ({ page: 'settings', data: { } }) },
-  { path: '/home', action: (req) => ({ page: 'home', data: { req, action: 'home' } }) },
+  { path: '/', action: (req) => ({ page: 'home', data: { req, action: 'home' } }) },
   { path: '/feed', action: (req) => ({ page: 'home', data: { req, action: 'feed' } }) },
+  { path: '/tags/:tag', action: (req) => ({ page: 'home', data: { req, action: 'tag' } }) },
   { path: '(.*)', action: () => ({ page: 'page3' }) },
 ])
