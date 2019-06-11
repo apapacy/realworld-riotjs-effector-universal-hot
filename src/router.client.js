172d856d6922b5c2f14899fe6564e1dfeb32f0f4
Андрей Овчаренко <ovcharenkoav@meta.ua>
2019-06-11 13:35:04 +0300

    switch to state in expression

--- src/router.client.js
+++ src/router.client.js
@@ -12,15 +12,28 @@ const render = async (location) => {
     const route = await router.resolve(location);
     const component = await import(`./riot/pages/${route.page}.riot`);
     try {
+      riot.unregister(route.page)
+    } catch (ex) {
+      console.log(ex)
+    }
+    try {
       riot.register(route.page, component.default || component)
     } catch (ex) {
       console.log(ex)
     }
     console.log('component', component)
     if (component.default.exports && component.default.exports.init) {
+      alert('init')
       await component.default.exports.init({...route, store: root.state.store});
     }
-    root.update(route)
+    console.log(root)
+    alert(1)
+    try {
+      root.update(route, root)
+    } catch (ex) {
+      console.log(ex)
+    }
+    alert(2)
 };
 
 history.listen(render);
