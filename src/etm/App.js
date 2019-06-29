ddf14f684333785f9dd3d2dfdbe0013f85716684
apaapcy <apapacy@gmail.com>
2019-06-29 13:38:21 +0300

    btxr

--- src/etm/App.js
+++ src/etm/App.js
@@ -4,6 +4,5 @@ import Layout from './components/layout'
 export default function (props) {
   return e('component', m(props, { id: 'app' }), [
     Layout(m(props)),
-    t('========================')
   ])
 }
